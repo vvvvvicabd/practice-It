d8ca6646657a7da94586c28adf3e96a1646ff1d7
Xinyi Xiang <xinyixiang1129@gmail.com>
2019-12-08 13:10:01 -0800

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -1,18 +1,18 @@
-public ListNode reverse(){
-    ListNode curr = front;
-   
-    if(curr == null){
-        return front;
-    }
-    ListNode back = null;
-    while(curr.next!=null){
-      ListNode temp = front.next;
-      front = front.next;      
-      curr.next = back;
-      back = curr;   
-      temp = temp.next;  
-      curr = front;       
-    }
-    curr.next = back;
-    return back;  
+// Write a method reverse that reverses the order of the elements in the list. For example, if the variable list initially stores this sequence of integers:
+
+// [1, 8, 19, 4, 17]
+// It should store the following sequence of integers after reverse is called:
+
+// [17, 4, 19, 8, 1]
+
+public void reverse(){
+    ListNode back = null;
+    
+    while(front!=null){
+      ListNode temp = front.next;
+      front.next = back;     
+      back = front;
+      front = temp;
+    }
+    front = back; 
 }
